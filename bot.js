fc25b18ce1c142bf907354833de9c8b67c4b045f
Zach_FR <zac232003@icloud.com>
2020-06-22 23:18:09 -0400

    added an join message embed

--- bot.js
+++ bot.js
@@ -13,7 +13,15 @@ client.on('message', msg => {
         msg.reply('pong!');
     }else if (msg.content === prefix + 'prefix') {
         msg.reply('The currently prefix is ' + prefix);
+    } else if (msg.content === prefix + 'join message') {
+        const embed = new Discord.MessageEmbed()
+            .setTitle('Join Message plugin')
+            .setURL('https://songoda.com/marketplace/product/join-message-joinmessage.378')
+            .setColor(0xff9800)
+            .setDescription('Custom join/leave message')
+            .setFooter('Zach_FRs Plugin');
+        msg.channel.send(embed);
     }
 });
 
-client.login('NzI0NzcwODg1MzgyMzA3ODky.XvFEOQ.ZL9yMT254tEgZlfJHsYTgZY4Q7A');
+client.login('NzI0NzcwODg1MzgyMzA3ODky.XvFp1g.1Foj0NpF_8vgiux8spMyM-380UM');
