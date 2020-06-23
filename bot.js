f554f4aeace529e54df08c06317db5706ea302f7
Zach_FR <zac232003@icloud.com>
2020-06-23 08:56:37 -0400

    e

--- bot.js
+++ bot.js
@@ -15,27 +15,33 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if (msg.content === prefix + 'ping') {
-        msg.reply('pong!');
-    }else if (msg.content === prefix + 'prefix') {
+    if (msg.content === prefix + 'prefix') {
         msg.reply('The currently prefix is ' + prefix);
-    } else if (msg.content === prefix + 'join message') {
-        const embed = new Discord.MessageEmbed()
-            .setTitle('Join Message plugin')
-            .setURL('https://songoda.com/marketplace/product/join-message-joinmessage.378')
-            .setColor(0xff9800)
-            .setDescription('Custom join/leave message')
-            .setFooter('Zach_FRss Plugin');
-        msg.channel.send(embed);
     } else if (msg.content === prefix + 'setprefix') {
         prefix = '/';
-    } else if (msg.content === prefix + 'version') {
+    } else if (msg.content === prefix + 'info') {
         msg.channel.send(info);
+    } else if (msg.content === prefix + 'joinmessage') {
+        msg.channel.send(joinmessage);
     }
 });
 
 const info = new Discord.MessageEmbed()
     .setTitle('Bot information')
     .setColor(0xff9800)
+    .setDescription(DESCRIPTION)
+    .addField('Author', AUTHOR, true)
+    .addField('Version', VERSION, true)
+    /*.addFields(
+        {name: '1', value:'d'},
+        {name: '\u200b', value:'\u200b'},
+    )*/
+const joinmessage = new Discord.MessageEmbed()
+    .setTitle('Join Message plugin')
+    .setURL('https://songoda.com/marketplace/product/join-message-joinmessage.378')
+    .setColor(0xff9800)
+    .setDescription('Custom join/leave message')
+    .setFooter('Zach_FRss Plugin');
+
 
 client.login(TOKEN);
