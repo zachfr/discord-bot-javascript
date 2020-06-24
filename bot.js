1ff73aad6adea884edb33ac4fb883b1a510aa4b5
Zach_FR <zac232003@icloud.com>
2020-06-24 12:26:53 -0400

    just a big update dont juge code please

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
+const node = require('nodeactyl');
 const client = new Discord.Client();
+const application = node.Client;
 const fetch = require("node-fetch");
 var auth = require('./auth.json');
 var package = require('./package.json');
@@ -13,6 +15,7 @@ var ACTIVITY = '';
 var AUTHOR = package.author;
 var DESCRIPTION = package.description;
 var UPTIME = client.uptime;
+
 var options = {
     'method': 'GET',
     'url': 'https://songoda.com/api/v2/products/join-message-joinmessage',
@@ -27,6 +30,11 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setActivity(ACTIVITY, { type: 'WATCHING' });
 });
+
+application.login(auth.HOST, auth.API, (logged_in, msg) => {
+    console.log(logged_in);
+});
+
 
 client.on('message', msg => {
     const args = msg.content.split(" ");
@@ -82,7 +90,7 @@ client.on('message', msg => {
 			console.error(err);
 			msg.channel.send('There was an error trying to purge messages in this channel!');
 		});
-	} else if (command === 'help') {
+	} else if (command === prefix + 'help') {
         msg.channel.send(help);
     } else if (command === prefix + 'songoda') {
        if(args.length >= 2){
@@ -119,6 +127,165 @@ client.on('message', msg => {
           .setDescription("Usage: !!songoda plugin <Plugin>");
           msg.channel.send(eb);
         }
+    } else if (command === prefix + 'server') {
+        if (args[0] === 'stats') {
+            if (args[1] === '1.15') {
+                application.getServerInfo('7263b879').then(response => {
+                    application.getRAMUsage('7263b879').then(ram => {
+                        application.getDiskUsage('7263b879').then(disk => {
+                            application.getCPUUsage('7263b879').then(cpu => {
+                                let ab = new Discord.MessageEmbed()
+                                    ab.setDescription('Minecraft server 1.15.2')
+                                    ab.addField("❯ Name of server", response.attributes.name, true);
+                                    ab.addField("❯ Max Memory", `${ram.current}/${response.attributes.limits.memory}GB`, true);
+                                    ab.addField("❯ Max Disk space", `${disk.current}/${response.attributes.limits.disk}MB`, true);
+                                    ab.addField("❯ Cpu usage", `${cpu.current}%`, true);
+                                msg.channel.send(ab);
+                            })
+                        })
+                    })
+                })
+            } else if (args[1] === '1.12') {
+                application.getServerInfo('355c9866').then(response => {
+                    application.getRAMUsage('355c9866').then(ram => {
+                        application.getDiskUsage('355c9866').then(disk => {
+                            application.getCPUUsage('355c9866').then(cpu => {
+                                let ab = new Discord.MessageEmbed()
+                                    ab.setDescription('Minecraft server 1.15.2')
+                                    ab.addField("❯ Name of server", response.attributes.name, true);
+                                    ab.addField("❯ Max Memory", `${ram.current}/${response.attributes.limits.memory}GB`, true);
+                                    ab.addField("❯ Max Disk space", `${disk.current}/${response.attributes.limits.disk}MB`, true);
+                                    ab.addField("❯ Cpu usage", `${cpu.current}%`, true);
+                                msg.channel.send(ab);
+                            })
+                        })
+                    })
+                })
+            } else if (args[1] === '1.8') {
+                application.getServerInfo('9b3290a8').then(response => {
+                    application.getRAMUsage('9b3290a8').then(ram => {
+                        application.getDiskUsage('9b3290a8').then(disk => {
+                            application.getCPUUsage('9b3290a8').then(cpu => {
+                                let ab = new Discord.MessageEmbed()
+                                    ab.setDescription('Minecraft server 1.15.2')
+                                    ab.addField("❯ Name of server", response.attributes.name, true);
+                                    ab.addField("❯ Max Memory", `${ram.current}/${response.attributes.limits.memory}GB`, true);
+                                    ab.addField("❯ Max Disk space", `${disk.current}/${response.attributes.limits.disk}MB`, true);
+                                    ab.addField("❯ Cpu usage", `${cpu.current}%`, true);
+                                msg.channel.send(ab);
+                            })
+                        })
+                    })
+                })
+            } else if (args[1] === 'bot') {
+                application.getServerInfo('0935a9c9').then(response => {
+                    application.getRAMUsage('0935a9c9').then(ram => {
+                        application.getDiskUsage('0935a9c9').then(disk => {
+                            application.getCPUUsage('0935a9c9').then(cpu => {
+                                let ab = new Discord.MessageEmbed()
+                                    ab.setDescription('Minecraft server 1.15.2')
+                                    ab.addField("❯ Name of server", response.attributes.name, true);
+                                    ab.addField("❯ Max Memory", `${ram.current}/${response.attributes.limits.memory}GB`, true);
+                                    ab.addField("❯ Max Disk space", `${disk.current}/${response.attributes.limits.disk}MB`, true);
+                                    ab.addField("❯ Cpu usage", `${cpu.current}%`, true);
+                                msg.channel.send(ab);
+                            })
+                        })
+                    })
+                })
+            }
+        } else if (args[0] === 'kill') {
+            if (args[1] === '1.15') {
+                application.killServer('7263b879').then(response => {
+                    msg.channel.send(`You have successfully kill server ${args[1]}, ${msg.author}!`);
+                })
+            } else if (args[1] === '1.12') {
+                application.killServer('355c9866').then(response => {
+                    msg.channel.send(`You have successfully kill server ${args[1]}, ${msg.author}!`);
+                })
+            } else if (args[1] === '1.8') {
+                application.killServer('9b3290a8').then(response => {
+                    msg.channel.send(`You have successfully kill server ${args[1]}, ${msg.author}!`);
+                })
+            } else if (args[1] === 'bot') {
+                application.killServer('0935a9c9').then(response => {
+                    msg.channel.send(`You have successfully kill server ${args[1]}, ${msg.author}!`);
+                })
+            } else {
+                msg.channel.send(`You didn't provide server, ${msg.author}!`);
+            }
+        } else if (args[0] === 'stop') {
+            if (args[1] === '1.15') {
+                application.stopServer('7263b879').then(response => {
+                    msg.channel.send(`You have successfully stop server ${args[1]}, ${msg.author}!`);
+                })
+            } else if (args[1] === '1.12') {
+                application.stopServer('355c9866').then(response => {
+                    msg.channel.send(`You have successfully stop server ${args[1]}, ${msg.author}!`);
+                })
+            } else if (args[1] === '1.8') {
+                application.stopServer('9b3290a8').then(response => {
+                    msg.channel.send(`You have successfully stop server ${args[1]}, ${msg.author}!`);
+                })
+            } else if (args[1] === 'bot') {
+                application.stopServer('0935a9c9').then(response => {
+                    msg.channel.send(`You have successfully stop server ${args[1]}, ${msg.author}!`);
+                })
+            } else {
+                msg.channel.send(`You didn't provide server, ${msg.author}!`);
+            }
+        } else if (args[0] === 'start') {
+            if (args[1] === '1.15') {
+                application.startServer('7263b879').then(response => {
+                    msg.channel.send(`You have successfully start server ${args[1]}, ${msg.author}!`);
+                })
+            } else if (args[1] === '1.12') {
+                application.startServer('355c9866').then(response => {
+                    msg.channel.send(`You have successfully start server ${args[1]}, ${msg.author}!`);
+                })
+            } else if (args[1] === '1.8') {
+                application.startServer('9b3290a8').then(response => {
+                    msg.channel.send(`You have successfully start server ${args[1]}, ${msg.author}!`);
+                })
+            } else if (args[1] === 'bot') {
+                application.startServer('0935a9c9').then(response => {
+                    msg.channel.send(`You have successfully start server ${args[1]}, ${msg.author}!`);
+                })
+            } else {
+                msg.channel.send(`You didn't provide server, ${msg.author}!`);
+            }
+        } else if (args[0] === 'restart') {
+            if (args[1] === '1.15') {
+                application.restartServer('7263b879').then(response => {
+                    msg.channel.send(`You have successfully restart server ${args[1]}, ${msg.author}!`);
+                })
+            } else if (args[1] === '1.12') {
+                application.restartServer('355c9866').then(response => {
+                    msg.channel.send(`You have successfully restart server ${args[1]}, ${msg.author}!`);
+                })
+            } else if (args[1] === '1.8') {
+                application.restartServer('9b3290a8').then(response => {
+                    msg.channel.send(`You have successfully restart server ${args[1]}, ${msg.author}!`);
+                })
+            } else if (args[1] === 'bot') {
+                application.restartServer('0935a9c9').then(response => {
+                    msg.channel.send(`You have successfully restart server ${args[1]}, ${msg.author}!`);
+                })
+            } else {
+                msg.channel.send(`You didn't provide server, ${msg.author}!`);
+            }
+        } else if (args[0] === 'exe') {
+            if (args[1] === '1.15') {
+                application.sendCommand('7263b879', `${args[2]} ${args[3]}`).then(response => {
+                    msg.channel.send(`You have successfully send commands on server ${args[1]}, ${args[2]} ${args[3]}, ${msg.author}!`);
+                })
+            }
+        } else if (args[0] === 'help') {
+            msg.channel.send(helpserver);
+        } else {
+            msg.channel.send(helpserver);
+        }
+            
     }
 });
 
@@ -129,7 +296,7 @@ const info = new Discord.MessageEmbed()
     .addField('Author', AUTHOR, true)
     .addField('Version', VERSION, true)
     .addField('Uptime', UPTIME, true)
-    .setFooter('Zach_FRs Plugin');
+    .setFooter("Zach_FR's plugin");
     /*.addFields(
         {name: '1', value:'d'},
         {name: '\u200b', value:'\u200b'},
@@ -139,9 +306,20 @@ const joinmessage = new Discord.MessageEmbed()
     .setURL('https://songoda.com/marketplace/product/join-message-joinmessage.378')
     .setColor(0xff9800)
     .setDescription('Custom join/leave message')
-    .setFooter('Zach_FRs Plugin');
+    .setFooter("Zach_FR's plugin");
 
 const help = new Discord.MessageEmbed()
     .setTitle('Help center')
-    .setFooter('Zach_FRs plugin');
+    .setFooter("Zach_FR's plugin");
+
+const helpserver = new Discord.MessageEmbed()
+    .setTitle('Help center (Server)')
+    .addField("❯ !server stats <name>", 'Show information on server', true)
+    .addField("❯ !server kill <name>", 'Kill server', true)
+    .addField("❯ !server start <name>", 'Start server', true)
+    .addField("❯ !server restart <name>", 'Restart server', true)
+    .addField("❯ !server stop <name>", 'Stop server', true)
+    .addField("❯ !server exe <name> <command>", 'Send a command on server', true)
+    .addField("❯ Name of server", '1.15, 1.12, 1.8, bot', true)
+    .setFooter("Zach_FR's plugin");
 client.login(TOKEN);
